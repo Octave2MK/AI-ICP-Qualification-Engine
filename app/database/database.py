f6da5318d21fa458be20d39cd782394e42774066
mk_user <mkusersgoat25@gmail.com>
2026-08-11 23:29:27 +0100

    Optimized and StartSearxngIntegration

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config.settings import DATABASE_URL
+from app.core.settings import DATABASE_URL
 import os
 DEBUG_SQL = os.getenv("SQL_DEBUG", "false").lower() == "true"
 
